4751729096cb3a7d1c2dd9a8f560245fa886c509
Kartik Mishra <38079315+kartikmishra@users.noreply.github.com>
2019-05-14 14:09:26 +0530

    Changes

--- app/src/main/java/com/example/qrcodescannerapp/MainActivity.java
+++ app/src/main/java/com/example/qrcodescannerapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.qrcodescannerapp;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.speech.tts.TextToSpeech;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,63 +29,68 @@ public class MainActivity extends AppCompatActivity {
     private CodeScanner mCodeScanner;
     Activity activity ;
     TextToSpeech t1;
-
+    private final int PERMISSION_REQUEST_CAMERA = 101;
+    private  CodeScannerView scannerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        scannerView = findViewById(R.id.scanner_view);
         activity = this;
 
 
-        final CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        if(askForCameraPermission()){
 
+            mCodeScanner = new CodeScanner(this, scannerView);
+            readQRCode();
 
-
-        if (ContextCompat.checkSelfPermission(getApplicationContext(),
-                android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{android.Manifest.permission.CAMERA}, 50);
 
         }
 
-            t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
 
-                @Override
-                public void onInit(int status) {
-                    if(status != TextToSpeech.ERROR) {
 
-                        t1.setLanguage(Locale.UK);
+
+    }
+
+
+    public void readQRCode(){
+
+
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        Toast.makeText(activity, result.getText(), Toast.LENGTH_LONG).show();
+                        t1.speak(result.getText(), TextToSpeech.QUEUE_FLUSH, null);
                     }
-                }
-            });
+                });
 
+                scannerView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
 
-            mCodeScanner = new CodeScanner(this, scannerView);
+                        mCodeScanner.startPreview();
+                    }
+                });
+            }
+        });
 
-            mCodeScanner.setDecodeCallback(new DecodeCallback() {
-                @Override
-                public void onDecoded(@NonNull final Result result) {
-
-                    activity.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-
-                            Toast.makeText(activity, result.getText(), Toast.LENGTH_LONG).show();
-                            t1.speak(result.getText(), TextToSpeech.QUEUE_FLUSH, null);
-                        }
-                    });
-
-                    scannerView.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-
-                            mCodeScanner.startPreview();
-                        }
-                    });
-                }
-            });
 
     }
 
@@ -93,6 +103,83 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public boolean askForCameraPermission(){
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.CAMERA)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Camera access needed");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("please confirm Camera access");//TODO put real question
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(
+                                    new String[]
+                                            {Manifest.permission.CAMERA}
+                                    , PERMISSION_REQUEST_CAMERA);
+                        }
+                    });
+                    builder.show();
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.CAMERA},
+                            PERMISSION_REQUEST_CAMERA);
+
+
+                }
+            }else{
+                return true;
+
+            }
+        }
+        else{
+
+            readQRCode();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    readQRCode();
+                    // permission was granted, yay! Do the
+
+                } else {
+                    Toast.makeText(this, "No Permissions ", Toast.LENGTH_SHORT).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
 
     @Override
     protected void onPause() {
